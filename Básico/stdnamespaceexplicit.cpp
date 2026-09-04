c25deaaefcb33d2df9d736eb8e94f09c9385cef7
DermRach <contato@junqueiraconsultoria.net.br>
2026-09-04 07:26:45 -0300

    Create stdnamespaceexplicit.cpp with example code
    Added a simple C++ program demonstrating the use of 'using namespace std'.

---
+++ Básico/stdnamespaceexplicit.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+// Namespace standart faz uso de diversas palavras reservadas
+// Para programas pequenos não tem problema
+// Boas práticas é bom ocultá-lo e usar std::
+
+using namespace std;
+
+int main()
+{
+	string name = "Sem std:: usando o using namespace std";
+	return 0;
+}
