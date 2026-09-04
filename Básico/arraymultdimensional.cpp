bf6be60c8c11924b50cecf8206bd72f9a4b10489
DermRach <contato@junqueiraconsultoria.net.br>
2026-09-04 11:58:12 -0300

    Add 2D array example for country-capital mapping
    Implement a 2D array to simulate a HashMap with country-capital pairs and output them.

---
+++ Básico/arraymultdimensional.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+// Matriz dimensional
+// 2 Linhas e 2 colunas
+// Esse mesmo exemplo vai ser usado para simular um HashMap
+
+std::string mapa[2][2] = {
+	{"Brasil", "Rio de Janeiro"},
+	{"Japao", "Toquio"}
+};
+
+int main()
+{
+	// Ele pecorre primeiro a primeira linha
+	// Linha 0 coluna 0
+	// Linha 0 coluna 1
+	// Por conversão a contagem começa no zero
+	for (int i = 0; i <= 1; i++)
+	{
+		for (int j = 0; j <= 1; j++)
+		{
+			// Output Brasil Rio de Janeiro Japao Toquio
+			// \ sozinho é escape, com outro \ garante ser \ 
+			std::cout << "Pais\\Capital: " << mapa[i][j] << "\n";
+		}
+	}
+
+	return 0;
+}
