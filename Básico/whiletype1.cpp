9ef8f89ad6e9b208c8203faca5d71ed4cecc793d
DermRach <contato@junqueiraconsultoria.net.br>
2026-09-05 00:53:28 -0300

    Create whiletype1.cpp

---
+++ Básico/whiletype1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+// Uso do while com contador tipo 1
+
+int main()
+{
+	int count = 0;
+
+	while (count <= 10)
+	{
+		count++;
+		std::cout << count << "\n";
+	}
+
+	return 0;
+}
