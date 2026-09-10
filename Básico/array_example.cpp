7ec0bb606d57724281906d675f1768cc5a9fc830
DermRach <contato@junqueiraconsultoria.net.br>
2026-09-10 18:18:14 -0300

    Update and rename vetorexemplo1.cpp to array_example.cpp

--- Básico/array_example.cpp
+++ Básico/array_example.cpp
@@ -2,19 +2,19 @@
 #include<Windows.h>
 #include<typeinfo>
 
-int x[5] = { 1,2,3,4,5 };
+int numbers[5] = { 1,2,3,4,5 };
 
 // Array\Vetor unidimensional
 // Acesso a elemento por indice
-// Medir tamanho 
+// Medir tamanho
 
 int main()
 {
 	SetConsoleOutputCP(CP_UTF8);
-	std::cout << "A posição " << 0 << " é " << x[0] << "\n";
-	int size = sizeof(x) / sizeof(x[0]);
+	std::cout << "A posição " << 0 << " é " << numbers[0] << "\n";
+	int size = sizeof(numbers) / sizeof(numbers[0]);
 
 	std::cout << "Tamanho do vetor: " << size << "\n";
-	std::cout << "Tipo de dado: " << typeid(x).name() << "\n";
+	std::cout << "Tipo de dado: " << typeid(numbers).name() << "\n";
 	return 0;
 }
