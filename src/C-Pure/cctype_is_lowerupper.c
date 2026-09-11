c86250f3959824f62cbc8c7860b6e753b8b0b642
DermRach <contato@junqueiraconsultoria.net.br>
2026-09-11 02:44:34 -0300

    Add cctype_is_lowerupper.c for case checking
    Implement a program to check if a character is upper or lower case.

---
+++ src/C-Pure/cctype_is_lowerupper.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include"cctype.h"
+
+// @date 02:41 11/09/2026
+// @author (DermRach) Rafael Junqueira
+// @website mundopauta.com.br
+// @brief Runtine for .header "cctype" locate main Include\.h
+// @Instructions use #include"cctype.h" isn't <cctype>
+// @AlternativeUse use a value direct on arguments islowerupper('d');
+
+int main()
+{
+	char inputchar;
+	printf("Input a letter use Upper ou Lower Case\n");
+	inputchar = getchar();
+	int triggercase = islowerupper(inputchar);
+
+	if (triggercase == -1)
+	{
+		printf("Please, input a letter.\n");
+	}
+	else
+	{
+		printf("%d", triggercase); // Se for um é Upper Case e se for zero é Lower Case
+	}
+	
+	return 0;
+}
