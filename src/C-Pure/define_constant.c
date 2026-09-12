5d07ebd44a1f4b3a136a53b1b83d2e6b8dd60745
DermRach <contato@junqueiraconsultoria.net.br>
2026-09-12 02:13:57 -0300

    Create define_constant.c with constant definitions
    Added a C program that defines and uses constants for LOW, MEDIUM, and HIGH levels.

---
+++ src/C-Pure/define_constant.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define LOW 0
+#define MEDIUM 1
+#define HIGH 2
+
+// Uso de define para fazer constantes
+// Não se usa memória
+
+int main()
+{
+	printf("%d", MEDIUM); // Imprime 0
+
+	return 0;
+}
