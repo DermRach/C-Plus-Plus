609b19f64da0f19cb24d0ad812f15404bed057b9
DermRach <contato@junqueiraconsultoria.net.br>
2026-09-12 04:57:45 -0300

    Add library.c with show and sum functions

---
+++ Include/.H/library.c
@@ -0,0 +1,13 @@
+#include"library.h";
+#include<stdio.h>
+
+void show(char nome[100])
+{
+	printf("%s", nome);
+}
+
+void sum(int valuea, int valueb)
+{
+	int result = valuea + valueb;
+	printf("%d\n", result);
+}
